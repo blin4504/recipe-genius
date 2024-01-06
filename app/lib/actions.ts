4abffadbb1b9e8c9dc82604a8beb0d1e7d83f0d7
blin4504 <bl4504@g.rit.edu>
2024-01-05 23:32:59 -0500

    chore: make sure api call works on server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,4 +8,19 @@ export async function createRecipe(formData: FormData) {
   };
 
   console.log(rawData);
+  try {
+    const response = await fetch("https://5urhvcrdr0.execute-api.us-east-1.amazonaws.com/dev/generate-recipe", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(rawData)
+    })
+
+    const jsonObj = await response.json();
+    console.log(jsonObj);
+    const obj = JSON.parse(jsonObj.body);
+    console.log(obj);
+
+  } catch (error) {
+    return `Error Message: ${error}`
+  }
 }
